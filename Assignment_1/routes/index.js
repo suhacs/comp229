5dcbf2dd70c018ed8c518485fd33bd5b26e98c2c
Suha Park <parksuha94@gmail.com>
2023-05-29 19:20:44 -0400

    Include the basic structure of the web page and initial design

---
+++ Assignment_1/routes/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use('/public', express.static(path.join(__dirname, '../public')));
+
+app.set('views', '../views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res, next) => {
+  res.render('index.ejs');
+});
+
+app.get('/aboutme', (req, res, next) => {
+  res.render('aboutme.ejs');
+});
+
+app.get('/project', (req, res, next) => {
+  res.render('project.ejs');
+});
+
+app.get('/service', (req, res, next) => {
+  res.render('service.ejs');
+});
+
+app.get('/contact', (req, res, next) => {
+  res.render('contact.ejs');
+});
+
+app.listen(8080);
