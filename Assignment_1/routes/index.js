54b6ed22d287339fd5e6d869f571e58fe264b2f9
Suha Park <parksuha94@gmail.com>
2023-05-30 14:54:39 -0400

    modify the path in the index.js

--- Assignment_1/routes/index.js
+++ Assignment_1/routes/index.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 app.use('/public', express.static(path.join(__dirname, '../public')));
 
-app.set('views', './views');
+app.set('views', path.join(__dirname, '../views'));
+// app.set('views', './views');
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res, next) => {
