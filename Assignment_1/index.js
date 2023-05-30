e500bfb8f3e22f6638ece5feaa963707c6ab3e7f
Suha Park <parksuha94@gmail.com>
2023-05-30 15:44:15 -0400

    modify path in the index.js

--- Assignment_1/index.js
+++ Assignment_1/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use('/public', express.static(path.join(__dirname, './public')));
+app.use('/public', express.static(path.join(__dirname, '../public')));
 
-app.set('views', path.join(__dirname, './views'));
+app.set('views', path.join(__dirname, '../views'));
 // app.set('views', './views');
 
 app.set('view engine', 'ejs');
