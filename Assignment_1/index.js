1cce266724b0017e152bec66a1f0a24335443fcd
Suha Park <parksuha94@gmail.com>
2023-05-30 16:29:03 -0400

    modify path

--- Assignment_1/index.js
+++ Assignment_1/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use('public', express.static(path.join(__dirname, '/public')));
+app.use('/public', express.static(path.join(__dirname, '/public')));
+console.log(path.join(__dirname, '/public'));
 
 app.set('views', path.join(__dirname, '/views'));
 // app.set('views', './views');
